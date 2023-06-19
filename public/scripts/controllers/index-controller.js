f6c2e9dba97c39be19a5c807f8eb24d27d8cdd83
cbue <christoph.buehlmann@ost.ch>
2023-06-19 15:21:15 +0200

    add login functionality

--- public/scripts/controllers/index-controller.js
+++ public/scripts/controllers/index-controller.js
@@ -1,3 +1,4 @@
+import TodoController from './todo-controller.js';
 import { authService } from '../services/auth-service.js';
 
 export default class IndexController {
@@ -13,18 +14,53 @@ export default class IndexController {
     this.#buttonFilterStatus = document.getElementById('buttonFilterStatus');
     this.#sortButtons = document.querySelectorAll('.sortButtons button');
 
+    this.#setLoginState();
     this.#buttonLogin.addEventListener('click', () => {
       // eslint-disable-next-line no-unused-expressions
       authService.isLoggedIn() ? this.#logout() : this.#login();
     });
   }
-  // eslint-disable-next-line class-methods-use-this
+
+  #setLoginButton() {
+    this.#buttonLogin.innerHTML = authService.isLoggedIn() ? 'Logout' : 'Login';
+  }
+
+  #setHeaderButtons() {
+    const state = authService.isLoggedIn() ? 'visible' : 'hidden';
+    this.#sortButtons.forEach((button) => {
+      // eslint-disable-next-line no-param-reassign
+      button.style.visibility = state;
+    });
+
+    this.#buttonCreateTodo.style.visibility = state;
+    this.#buttonFilterStatus.style.visibility = state;
+  }
+
+  #setLoginState() {
+    this.#setHeaderButtons();
+    this.#setLoginButton();
+  }
+
   #login() {
-    authService.login('admin@admin.ch', '123456');
+    authService
+      .login('admin@admin.ch', '123456')
+      .then(() => {
+        const todoController = new TodoController();
+        todoController.renderTodoView();
+        this.#buttonLogin.innerHTML = 'Logout';
+        this.#setLoginState();
+      })
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error('login error:', error);
+      });
   }
 
   #logout() {
     authService.logout();
+    this.#todoContainer.innerHTML = '';
+    this.#buttonLogin.innerHTML = 'Login';
+    this.#setLoginState();
   }
 }
 
